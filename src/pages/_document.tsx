f99bb1f3d61fb2fd8cac6eb6983c47f2d72c11ef
uSebazz <iSebazz@outlook.es>
2022-08-28 17:56:34 -0500

    perf: add font optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,26 +2,34 @@ import { ColorModeScript } from '@chakra-ui/react';
 import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-        const initialProps = await Document.getInitialProps(ctx);
-        return { ...initialProps };
-    }
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    <link rel='icon' href='/rex.png' />
-                    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-                    <link rel="manifest" href="/site.webmanifest" />
-                </Head>
-                <body>
-                    <ColorModeScript />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link rel="icon" href="/rex.png" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<style data-href="https://fonts.googleapis.com/css2?family=Inter&display=optional">
+						{`
+                          @font-face {
+                            font-family: 'Inter var';
+                            font-style: normal;
+                          }
+                       `}
+					</style>
+					<link rel="manifest" href="/site.webmanifest" />
+				</Head>
+				<body>
+					<ColorModeScript />
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
